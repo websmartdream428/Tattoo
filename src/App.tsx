2d3cc122d63d0a1f032daaa26476bbff22e4893e
websmartdream <webdream1108@gmail.com>
2022-02-14 10:23:14 +0800

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import "assets/css/App.css";
+
+function App() {
+  return (
+    <Router>
+      Coding here
+      {/* <Route exact path="/" component={LandingPage} /> */}
+    </Router>
+  );
+}
+
+export default App;
