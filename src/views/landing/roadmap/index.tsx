c4b0f5a7702f7cf4013b8d5e18b3d1605ad78a51
websmartdream <webdream1108@gmail.com>
2022-02-14 20:26:03 +0800

    feat: roadmap section

--- src/views/landing/roadmap/index.tsx
+++ src/views/landing/roadmap/index.tsx
@@ -1,7 +1,111 @@
 import React from "react";
+import {
+  RoadMapCard,
+  RoadMapCardGroup,
+  RoadMapCardWrapper,
+  RoadMapLine,
+  RoadMapSection,
+  RoadMapTitleSection,
+  RoadmapWrapper,
+} from "./roadmap.styled";
+
+import roadmapLine from "assets/image/roadmap/roadmap-line.png";
+import roadmap1 from "assets/image/roadmap/roadmap1.png";
+import roadmap2 from "assets/image/roadmap/roadmap2.png";
+import roadmap3 from "assets/image/roadmap/roadmap3.png";
+import roadmap4 from "assets/image/roadmap/roadmap4.png";
+import roadmap5 from "assets/image/roadmap/roadmap5.png";
 
 const RoadMapPart = () => {
-  return <div>RoadMapPart</div>;
+  return (
+    <RoadmapWrapper>
+      <RoadMapTitleSection>
+        <h2>Roadmap</h2>
+        <p>
+          {
+            "This roadmap outlines our goals and where we want to take The Tattoo Shop NFT. We have a lot of ideas and concepts that we are working on. It may evolve over time and hopefully become even better!"
+          }
+        </p>
+      </RoadMapTitleSection>
+      <RoadMapSection>
+        <RoadMapLine>
+          <img src={roadmapLine} className="rm-line" alt="roadmapLine" />
+          <div id="rm1">
+            <img src={roadmap1} alt="roadmap1" />
+          </div>
+          <div id="rm2">
+            <img src={roadmap2} alt="roadmap2" />
+          </div>
+          <div id="rm3">
+            <img src={roadmap3} alt="roadmap3" />
+          </div>
+          <div id="rm4">
+            <img src={roadmap4} alt="roadmap4" />
+          </div>
+          <div id="rm5">
+            <img src={roadmap5} alt="roadmap5" />
+          </div>
+        </RoadMapLine>
+        <RoadMapCardWrapper>
+          <RoadMapCardGroup>
+            <RoadMapCard>
+              <h2>Launch Roadmap</h2>
+              <p>
+                {
+                  "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat."
+                }
+              </p>
+            </RoadMapCard>
+            <div />
+          </RoadMapCardGroup>
+          <RoadMapCardGroup>
+            <div />
+            <RoadMapCard>
+              <h2>The Legend</h2>
+              <p>
+                {
+                  "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat."
+                }
+              </p>
+            </RoadMapCard>
+          </RoadMapCardGroup>
+          <RoadMapCardGroup>
+            <RoadMapCard>
+              <h2>Art Holds Keys</h2>
+              <p>
+                {
+                  "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat."
+                }
+              </p>
+            </RoadMapCard>
+            <div />
+          </RoadMapCardGroup>
+          <RoadMapCardGroup>
+            <div />
+            <RoadMapCard>
+              <h2>Art with Utility</h2>
+              <p>
+                {
+                  "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat."
+                }
+              </p>
+            </RoadMapCard>
+          </RoadMapCardGroup>
+          <RoadMapCardGroup>
+            <RoadMapCard>
+              <h2>Team expansion</h2>
+              <p>
+                {
+                  "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat."
+                }
+              </p>
+            </RoadMapCard>
+            <div />
+          </RoadMapCardGroup>
+        </RoadMapCardWrapper>
+      </RoadMapSection>
+    </RoadmapWrapper>
+  );
 };
 
 export default RoadMapPart;
---
+++ src/views/landing/roadmap/roadmap.styled.tsx
@@ -0,0 +1,182 @@
+import styled from "styled-components";
+import no1 from "assets/image/roadmap/no1.svg";
+import no2 from "assets/image/roadmap/no2.svg";
+import no3 from "assets/image/roadmap/no3.svg";
+import no4 from "assets/image/roadmap/no4.svg";
+import no5 from "assets/image/roadmap/no5.svg";
+
+export const RoadmapWrapper = styled.div`
+  padding: 170px 0;
+`;
+
+export const RoadMapTitleSection = styled.div`
+  h2,
+  p {
+    margin: 0;
+    text-align: center;
+  }
+  h2 {
+    font-family: GraublauWeb-bold;
+    color: #f4f4f4;
+    font-size: 62px;
+    text-align: center;
+    line-height: 62px;
+    -webkit-text-stroke: 1px #bc3a08;
+    margin-bottom: 32px;
+  }
+  p {
+    max-width: 596px;
+    width: 100%;
+    font-family: Gilroy-Regular;
+    font-size: 16px;
+    line-height: 24px;
+    margin: auto;
+    color: #b8b8b8;
+  }
+`;
+
+export const RoadMapSection = styled.div`
+  position: relative;
+  width: 100%;
+  margin-top: 50px;
+`;
+
+export const RoadMapLine = styled.div`
+  position: relative;
+  text-align: center;
+  width: 100%;
+  padding: 200px 0;
+  & > *:not(.rm-line) {
+    position: absolute;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    z-index: 1;
+    ::before {
+      position: absolute;
+      left: -15%;
+      top: -15%;
+      content: "";
+      width: 25%;
+      padding-top: 25%;
+      background-size: 100% 100%;
+    }
+    img {
+      width: 80%;
+    }
+    ::after {
+      position: absolute;
+      content: "";
+      width: 50%;
+      height: 100px;
+      filter: blur(87px);
+      z-index: -1;
+    }
+  }
+  .rm-line {
+    margin: auto;
+    width: 250px;
+  }
+  #rm1 {
+    top: 5%;
+    left: 47%;
+    ::after {
+      background: #d6d6d6;
+    }
+    ::before {
+      background-image: url(${no1});
+    }
+  }
+  #rm2 {
+    top: 23%;
+    left: 33%;
+    ::after {
+      background: #d6d6d6;
+    }
+    ::before {
+      background-image: url(${no2});
+    }
+  }
+  #rm3 {
+    top: 45%;
+    left: 47%;
+    ::after {
+      background: #912462;
+    }
+    ::before {
+      background-image: url(${no3});
+    }
+  }
+  #rm4 {
+    top: 65%;
+    left: 30%;
+    ::after {
+      background: #31bf90;
+    }
+    ::before {
+      background-image: url(${no4});
+    }
+  }
+  #rm5 {
+    top: 83%;
+    left: 50%;
+    ::after {
+      background: #ffe06f;
+    }
+    ::before {
+      background-image: url(${no5});
+      top: -10%;
+      left: -20%;
+    }
+  }
+`;
+
+export const RoadMapCardWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  padding-top: 7%;
+  width: 100%;
+  & > *:not(:first-child) {
+    margin-top: 10%;
+  }
+`;
+
+export const RoadMapCardGroup = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const RoadMapCard = styled.div`
+  width: 31%;
+
+  h2,
+  p {
+    margin: 0;
+  }
+  h2 {
+    margin-bottom: 47px;
+    position: relative;
+    font-family: GraublauWeb;
+    font-size: 48px;
+    line-height: 55px;
+    text-transform: capitalize;
+    color: #f4f4f4;
+    white-space: nowrap;
+    ::after {
+      content: "";
+      width: 70%;
+      position: absolute;
+      bottom: -17px;
+      left: 0;
+      height: 3px;
+      background: linear-gradient(270.03deg, #bc3a08 0.05%, #941a0b 100%);
+    }
+  }
+  p {
+    color: #b8b8b8;
+    font-family: Gilroy-Regular;
+    font-size: 16px;
+    line-height: 24px;
+  }
+`;
